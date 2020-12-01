4fe1594ed7b8d3ffa7aabc42441bd9e5d471bc8b
carlohera <46058261+carlohera@users.noreply.github.com>
2020-12-01 13:36:31 +0200

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,8 @@
+function showDiv() {
+    var T = document.getElementById("about");
+    T.style.display = "block";  // <-- Set it to block
+}
+function showDiv2() {
+    var T = document.getElementById("staff");
+    T.style.display = "block";  // <-- Set it to block
+}
